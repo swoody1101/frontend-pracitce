9d3affcf293dee338eebf91330fb1c4e630ae31d
leesu <leesu@ictway.co.kr>
2025-01-13 17:10:59 +0900

    export/import 실습 예제 작성

---
+++ React/02 JS Refresher/assets/scripts/app.js
@@ -0,0 +1,10 @@
+// import { apiKey } from "./util.js";
+
+// import apiKey from "./util.js";
+import { apiKey, abc as content } from "./util.js";
+// import * as util from "./util.js";
+
+// console.log(apiKey);
+// console.log(abc);
+// console.log(util.apiKey);
+console.log(content);
---
+++ React/02 JS Refresher/assets/scripts/util.js
@@ -0,0 +1,4 @@
+// export let apiKey = "anasodoaeofdw1";
+export default "aksdladlskajdksl";
+export let apiKey = "adanksdljofiw1";
+export let abc = "abc";
