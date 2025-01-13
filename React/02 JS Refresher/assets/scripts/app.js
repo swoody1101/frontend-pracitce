b583a097b34dd226ab4e00239c0848798432662d
swoody1101 <swoody1101@gamil.com>
2025-01-13 17:19:57 +0900

    import 항목 수정

--- React/02 JS Refresher/assets/scripts/app.js
+++ React/02 JS Refresher/assets/scripts/app.js
@@ -1,7 +1,7 @@
 // import { apiKey } from "./util.js";
 
 // import apiKey from "./util.js";
-import { apiKey, abc as content } from "./util.js";
+import { abc as content } from "./util.js";
 // import * as util from "./util.js";
 
 // console.log(apiKey);
