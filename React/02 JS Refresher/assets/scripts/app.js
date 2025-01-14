d9c053a9dcd09a94877577c53513d475678501d0
swoody1101 <swoody1101@gmail.com>
2025-01-14 14:29:02 +0900

    JS 실습 예제 작성

--- React/02 JS Refresher/assets/scripts/app.js
+++ React/02 JS Refresher/assets/scripts/app.js
@@ -2,9 +2,59 @@
 
 // import apiKey from "./util.js";
 // import * as util from "./util.js";
-import { abc as content } from "./util.js";
+// import { abc as content } from "./util.js";
 
 // console.log(apiKey);
 // console.log(abc);
 // console.log(util.apiKey);
-console.log(content);
+// console.log(content);
+
+// console.log("Hello wolrd!");
+
+// function greet() {
+//   console.log("Hello!");
+// }
+
+// greet();
+
+// const hobbies = ["a", "b", "c"];
+// hobbies.push("d");
+
+// const index = hobbies.findIndex((item) => item === "c");
+
+// console.log(index);
+
+// const editedHobbies = hobbies.map((item) => ({ text: item }));
+// console.log(editedHobbies);
+
+// const [firstName, lastName] = ["Sangu", "Lee"];
+// console.log(firstName);
+// console.log(lastName);
+
+// const { name: userName, age } = {
+//   name: "Max",
+//   age: 34,
+// };
+
+// console.log(userName);
+// console.log(age);
+
+const hobbies = ["Sports", "Cooking"];
+const user = {
+  name: "Max",
+  age: 34,
+};
+
+const newHobbies = ["Reading"];
+const mergedHobbies = [...hobbies, ...newHobbies];
+console.log(mergedHobbies);
+
+const extendedUser = {
+  isAdmin: true,
+  ...user,
+};
+console.log(extendedUser);
+
+for (const hobby of hobbies) {
+  console.log(hobby);
+}
