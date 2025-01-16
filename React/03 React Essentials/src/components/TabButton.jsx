1144a3f7f364ae70b84b5bff7a0d0b7ac10c5788
swoody1101 <swoody1101@gmail.com>
2025-01-16 09:19:49 +0900

    props childern 활용 실습 예제 작성

---
+++ React/03 React Essentials/src/components/TabButton.jsx
@@ -0,0 +1,14 @@
+export default function TabButton({ children }) {
+  return (
+    <li>
+      <button>{children}</button>
+    </li>
+  );
+}
+// export default function TabButton(props) {
+//     return (
+//       <li>
+//         <button>{props.children}</button>
+//       </li>
+//     );
+//   }
