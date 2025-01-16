2c7e6becb70597d4744f4e7278000c7df9b9148b
swoody1101 <swoody1101@gmail.com>
2025-01-16 09:45:35 +0900

    handleSelect함수를 Prop 속성으로 전달하는 실습 예제 작성

--- React/03 React Essentials/src/components/TabButton.jsx
+++ React/03 React Essentials/src/components/TabButton.jsx
@@ -1,11 +1,7 @@
-export default function TabButton({ children }) {
-  function handleClick() {
-    console.log('Hello Wolrd!');
-  }
-
+export default function TabButton({ children, onSelect }) {
   return (
     <li>
-      <button onClick={handleClick}>{children}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   );
 }
