84117ec09ab7174751807cf2ff793300c1e7564d
swoody1101 <swoody1101@gmail.com>
2025-01-16 09:30:56 +0900

    props.img에서 props.img로 변경

--- React/03 React Essentials/src/App.jsx
+++ React/03 React Essentials/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
             <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
-              img={CORE_CONCEPTS[0].image}
+              image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
--- React/03 React Essentials/src/components/CoreConcept.jsx
+++ React/03 React Essentials/src/components/CoreConcept.jsx
@@ -1,7 +1,7 @@
 export default function CoreConcept(props) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
+      <img src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
