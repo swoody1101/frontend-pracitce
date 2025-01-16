d362a5573e1b61bb158837a242b6d1caa8fd4441
swoody1101 <swoody1101@gmail.com>
2025-01-16 13:55:10 +0900

    CSS 스타일링 및 동적 스타일링 실습

--- React/03 React Essentials/src/components/TabButton.jsx
+++ React/03 React Essentials/src/components/TabButton.jsx
@@ -1,9 +1,11 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   console.log("TABBUTTON COMPONENT EXECUTING");
 
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
