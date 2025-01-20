bcb0f1375f45a29a1d49790761adc328d11196fe
swoody1101 <swoody1101@gmail.com>
2025-01-20 14:14:19 +0900

    Player 컴포넌트 분리

--- React/04 Tic Tac Toe/src/App.jsx
+++ React/04 Tic Tac Toe/src/App.jsx
@@ -1,16 +1,12 @@
+import Player from "./assets/components/Player";
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-name">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-name">O</span>
-          </li>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
       </div>
     </main>
