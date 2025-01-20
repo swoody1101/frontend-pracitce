68b0e8b47a17d3785ab0b44602d97587439892b6
swoody1101 <swoody1101@gmail.com>
2025-01-20 17:35:38 +0900

    게임판 컴포넌트 생성

--- React/04 Tic Tac Toe/src/assets/components/Player.jsx
+++ React/04 Tic Tac Toe/src/assets/components/Player.jsx
@@ -9,7 +9,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   function handleChange(event) {
-    console.log(event);
+    // console.log(event);
     setPlayerName(event.target.value);
   }
 
