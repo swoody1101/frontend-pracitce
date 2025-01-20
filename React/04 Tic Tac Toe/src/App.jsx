ec324d7b23256c585e9851a50c38d9233d2e19aa
swoody1101 <swoody1101@gmail.com>
2025-01-20 13:59:53 +0900

    player 명시 목록 생성

--- React/04 Tic Tac Toe/src/App.jsx
+++ React/04 Tic Tac Toe/src/App.jsx
@@ -1,10 +1,20 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-name">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-name">O</span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
