82996158e416a92c88fe1a6e9bed294db96e90d2
swoody1101 <swoody1101@gmail.com>
2025-01-21 10:02:37 +0900

    보드 게임 수행 로직 추가

--- React/04 Tic Tac Toe/src/App.jsx
+++ React/04 Tic Tac Toe/src/App.jsx
@@ -1,15 +1,34 @@
+import { useState, useSyncExternalStore } from "react";
+
 import Player from "./assets/components/Player.jsx";
 import GameBoard from "./assets/components/GameBoard.jsx";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
   );
--- React/04 Tic Tac Toe/src/assets/components/GameBoard.jsx
+++ React/04 Tic Tac Toe/src/assets/components/GameBoard.jsx
@@ -6,17 +6,21 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = "X";
-      return updatedBoard;
-    });
+    if (!gameBoard[rowIndex][colIndex]) {
+      setGameBoard((prevGameBoard) => {
+        const updatedBoard = [
+          ...prevGameBoard.map((innerArray) => [...innerArray]),
+        ];
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+        return updatedBoard;
+      });
+
+      onSelectSquare();
+    }
   }
 
   return (
