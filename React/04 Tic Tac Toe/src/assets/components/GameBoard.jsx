fa45dd02cf1b84041fc68d88bee55c454b118305
swoody1101 <swoody1101@gmail.com>
2025-01-21 09:25:41 +0900

    게임 보드 불변 객체 상태로 업데이트

--- React/04 Tic Tac Toe/src/assets/components/GameBoard.jsx
+++ React/04 Tic Tac Toe/src/assets/components/GameBoard.jsx
@@ -1,24 +1,39 @@
+import { useState } from "react";
+
 const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
-  
-  export default function GameBoard() {
-    return (
-      <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => (
-          <li key={rowIndex}>
-            <ol>
-              {row.map((playerSymbol, colIndex) => (
-                <li key={colIndex}>
-                  <button>{playerSymbol}</button>
-                </li>
-              ))}
-            </ol>
-          </li>
-        ))}
-      </ol>
-    );
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
   }
-  
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
