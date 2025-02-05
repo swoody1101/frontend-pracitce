1942f27b4261d63b77ca8827b6c8bb69754fd329
swoody1101 <swoody1101@gmail.com>
2025-02-05 16:14:57 +0900

    totalInterest 관련 오류 수정

--- React/05 Essentials Practice/src/components/Result.jsx
+++ React/05 Essentials Practice/src/components/Result.jsx
@@ -21,14 +21,14 @@ export default function Results({ input }) {
       <tbody>
         {resultsData.map((yearData) => {
           const totalInterest =
-            yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
+            yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
           const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
 
           return (
             <tr key={yearData.year}>
               <td>{yearData.year}</td>
               <td>{formatter.format(yearData.valueEndOfYear)}</td>
-              <td>{yearData.interest}</td>
+              <td>{formatter.format(yearData.interest)}</td>
               <td>{formatter.format(totalInterest)}</td>
               <td>{formatter.format(totalAmountInvested)}</td>
             </tr>
