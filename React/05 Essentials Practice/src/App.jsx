e0757eca00f0d85a7c8b1170c675cb0033d1b5b5
swoody1101 <swoody1101@gmail.com>
2025-02-05 15:17:21 +0900

    userInput 상태 App.jsx로 끌어올림

--- React/05 Essentials Practice/src/App.jsx
+++ React/05 Essentials Practice/src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIndentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIndentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {/* Results go here */}
     </>
   );
 }
