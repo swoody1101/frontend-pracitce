ce4794a273ce9c7c67c8739526739f1c665ed55d
swoody1101 <swoody1101@gmail.com>
2025-02-11 16:54:33 +0900

    useCallback 사용해서 handleRemovePlace 함수가 재생성 되지 않도록 방지

--- React/11 Side Effects useEffect/src/App.jsx
+++ React/11 Side Effects useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -57,7 +57,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -68,7 +68,7 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
