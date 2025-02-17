dbdc49389aca85247ac7cc39510af4f5ec7ef2c5
swoody1101 <swoody1101@gmail.com>
2025-02-17 17:27:01 +0900

    답변 선택 후 나머지 버튼 disabled 되도록 속성 추가

--- React/12 Demo Project - React Quiz/src/components/Answers.jsx
+++ React/12 Demo Project - React Quiz/src/components/Answers.jsx
@@ -32,7 +32,11 @@ export default function Answers({
 
         return (
           <li key={answer} className="answer">
-            <button onClick={() => onSelect(answer)} className={cssClass}>
+            <button
+              onClick={() => onSelect(answer)}
+              className={cssClass}
+              disabled={answerState !== ""}
+            >
               {answer}
             </button>
           </li>
