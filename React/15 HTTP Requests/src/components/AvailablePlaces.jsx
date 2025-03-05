2d7fe5ece4ba8a61bf4e86089f48bd2ac83cd203
swoody1101 <swoody1101@gmail.com>
2025-03-05 16:30:28 +0900

    이미지 로딩 상태 표현

--- React/15 HTTP Requests/src/components/AvailablePlaces.jsx
+++ React/15 HTTP Requests/src/components/AvailablePlaces.jsx
@@ -5,13 +5,16 @@ import Places from "./Places.jsx";
 const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -21,6 +24,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoadings={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
