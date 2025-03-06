451dcf684257a6c56a4db876b89ea314b5d2e768
swoody1101 <swoody1101@gmail.com>
2025-03-06 17:24:17 +0900

    백엔드 통신 에러 처리

--- React/15 HTTP Requests/src/components/AvailablePlaces.jsx
+++ React/15 HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,24 +1,44 @@
 import { useEffect, useState } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An eroor occurred!" message={error.message} />;
+  }
 
   return (
     <Places
