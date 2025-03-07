f143057f367137a4f72bcdeb4644101df20f0a9a
swoody1101 <swoody1101@gmail.com>
2025-03-07 09:40:33 +0900

    fetch 된 데이터 내 위치를 기준으로 정렬

--- React/15 HTTP Requests/src/components/AvailablePlaces.jsx
+++ React/15 HTTP Requests/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 const places = localStorage.getItem("places");
 
@@ -22,15 +23,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Failed to fetch places");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || "Could not fetch places, please try again later",
         });
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
