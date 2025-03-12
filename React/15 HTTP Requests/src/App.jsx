d6356de269af84f9d1798d876d27af36e1885e46
swoody1101 <swoody1101@gmail.com>
2025-03-12 09:39:05 +0900

    저장된 장소를 불러오도록 기능 추가

--- React/15 HTTP Requests/src/App.jsx
+++ React/15 HTTP Requests/src/App.jsx
@@ -1,20 +1,39 @@
-import { useRef, useState, useCallback } from "react";
+import { useRef, useState, useCallback, useEffect } from "react";
 
 import Places from "./components/Places.jsx";
 import Modal from "./components/Modal.jsx";
 import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
-import { updateUserPlaces } from "./http.js";
+import { fetchUserPlaces, updateUserPlaces } from "./http.js";
 import Error from "./components/Error.jsx";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user places." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -55,7 +74,7 @@ function App() {
           (place) => place.id !== selectedPlace.current.id
         )
       );
-      
+
       try {
         await updateUserPlaces(
           userPlaces.filter((place) => place.id !== selectedPlace.current.id)
@@ -103,9 +122,12 @@ function App() {
         </p>
       </header>
       <main>
+        {error && <Error title="An error occurred!" message={error.message} />}
         <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          isLoading={isFetching}
+          loadingText="Fetching your places..."
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
